be66a1055ff9c9f4f1958ec63c15d69ad9147b1d
hcbelias <hcbe2004@gmail.com>
2015-01-31 13:37:16 -0200

    al;teracaoes navbar e cricao de aruqivo de variaceis less

--- client/app/carousel/carousel.controller.js
+++ client/app/carousel/carousel.controller.js
@@ -4,17 +4,20 @@ angular.module('ecomovaJsApp')
   .controller('CarouselCtrl', function ($scope) {
     $scope.interval = 100000;
   	$scope.slides = [{
-  		image: 'assets/images/bg1.jpg',
+  		backgroundImage: 'assets/images/bg1.jpg',
+      image: 'assets/images/iphone.png',
   		text: 'todos os dias',
   		header: 'Caronas para'
   	},
   	{
-  		image: 'assets/images/bg2.jpg',
+  		backgroundImage: 'assets/images/bg2.jpg',
+      image: 'assets/images/student.png',
   		text: 'a faculdade',
   		header: 'Caronas para'
   	},
   	{
-  		image: 'assets/images/bg3.jpg',
+  		backgroundImage: 'assets/images/bg3.jpg',
+      image: 'assets/images/businessman.png',
   		text: 'o trabalho',
   		header: 'Caronas para'
   	}];
