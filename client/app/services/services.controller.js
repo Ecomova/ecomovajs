604959a36dd0ea74aeb1439c9a07373d4c280b4a
hcbelias <hcbe2004@gmail.com>
2015-01-31 01:37:32 -0200

    newsletter

---
+++ client/app/services/services.controller.js
@@ -0,0 +1,18 @@
+   'use strict';
+
+angular.module('ecomovaJsApp')
+  .controller('ServicesCtrl', function ($scope) {
+	  $scope.items = [{
+	  		image: 'assets/images/carac1.jpg',
+	  		title: 'Flexível',
+	  		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi facilisis augue nunc, facilisis mattis turpis tincidunt a. Praesent tempus euismod semper. In et.'
+	  	},{
+	  		image: 'assets/images/carac2.jpg',
+	  		title: 'Confiável',
+	  		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi facilisis augue nunc, facilisis mattis turpis tincidunt a. Praesent tempus euismod semper. In et.'
+	  	},{
+	  		image: 'assets/images/carac3.jpg',
+	  		title: 'Seguro',
+	  		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi facilisis augue nunc, facilisis mattis turpis tincidunt a. Praesent tempus euismod semper. In et.'
+	  	}];
+  });
