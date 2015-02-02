d7b225ae4de4661f1aa364842d9aca3ed29b52f9
hcbelias <hcbe2004@gmail.com>
2015-02-02 00:08:32 -0200

    trying to pass to em/rem

--- client/app/carousel/carousel.controller.js
+++ client/app/carousel/carousel.controller.js
@@ -4,19 +4,19 @@ angular.module('ecomovaJsApp')
   .controller('CarouselCtrl', function ($scope) {
     $scope.interval = 100000;
   	$scope.slides = [{
-  		backgroundClass: 'background-everyday',
+  		backgroundClass: 'background-cover-everyday',
       image: 'assets/images/iphone.png',
   		text: 'todos os dias',
   		header: 'Caronas para'
   	},
   	{
-  		backgroundClass: 'background-college',
+  		backgroundClass: 'background-cover-college',
       image: 'assets/images/student.png',
   		text: 'a faculdade',
   		header: 'Caronas para'
   	},
   	{
-  		backgroundClass: 'background-work',
+  		backgroundClass: 'background-cover-work',
       image: 'assets/images/businessman.png',
   		text: 'o trabalho',
   		header: 'Caronas para'
