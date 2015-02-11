a14fdf50c8dbce4e6ff8e38e583baf2cade8cd44
hcbelias <hcbe2004@gmail.com>
2015-02-11 01:40:55 -0200

    fixes no contact, checkbox e fixes no carousel

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri:  'mongodb://ecomovaadmin:poioiu098@ds041831.mongolab.com:41831/ecomovadb'
+    //uri:  'mongodb://ecomovaadmin:poioiu098@ds041831.mongolab.com:41831/ecomovadb'
+    uri: 'mongodb://localhost/fullstack-dev'
   },
 
   seedDB: true
