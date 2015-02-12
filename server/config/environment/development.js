7e944e19e877e6cb6212710b847559792b1547b4
hcbelias <hcbe2004@gmail.com>
2015-02-12 00:08:57 -0200

    changes on

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,8 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    //uri:  'mongodb://ecomovaadmin:poioiu098@ds041831.mongolab.com:41831/ecomovadb'
-    uri: 'mongodb://localhost/fullstack-dev'
+    uri:  'mongodb://ecomovaadmin:poioiu098@ds041831.mongolab.com:41831/ecomovadb'
+    //uri: 'mongodb://localhost/fullstack-dev'
   },
 
   seedDB: true
